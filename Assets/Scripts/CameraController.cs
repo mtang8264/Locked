90eb509e0d93842b62c89157288db00cb671e7d8
Michael Polronieri Tang <mtang8264@gmail.com>
2019-10-09 10:31:06 -0400

    Created a new scene for outside and created a basic two camera system

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Camera bedroom;
+    public Camera outside;
+
+    public float threshold;
+
+    public Transform player;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(player.position.x < threshold)
+        {
+            outside.depth = 0;
+            bedroom.depth = 1;
+        }
+        else
+        {
+            outside.depth = 1;
+            bedroom.depth = 0;
+        }
+    }
+}
